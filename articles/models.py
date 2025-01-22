57207653d85510fae76d5cf8fab050d55d960d47
Dmitrij Serov <daserov1@mts.ru>
2025-01-23 00:04:13 +0300

    add method for crate article

--- articles/models.py
+++ articles/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    rating = models.FloatField(default=0.0)
 
     def __str__(self):
         return self.title
--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,28 @@
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 from .models import Article
 
+@api_view(['GET'])
 def get_feed(request):
-    articles = Article.objects.all().values('title', 'description')
-    return JsonResponse(list(articles), safe=False)
+    articles = Article.objects.all().values('title', 'description', 'rating')
+    return Response(list(articles))
 
-def get_some_text(request):
-    return JsonResponse("Hello world!", safe=False)
+@api_view(['POST'])
+def create_artice(request):
+    title = request.data.get('title')
+    description = request.data.get('description')
+    rating = request.data.get('rating')
+
+    if not title or not description or not rating:
+        return Response({"error": "Поля title, description, rating обязательны"}, status = status.HTTP_400_BAD_REQUEST)
+
+    article = Article.objects.create(title=title, description=description, rating=rating)
+
+    return Response({
+        "id": article.id,
+        "title": article.title,
+        "description": article.description,
+        "rating": article.rating,
+    }, status = status.HTTP_201_CREATED)
+
