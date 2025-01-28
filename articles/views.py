0a7d61196fb06fe4e75030ae8043c0374b83fe4e
Dmitrij Serov <daserov1@mts.ru>
2025-01-28 23:25:15 +0300

    refactoring to serializer

--- articles/views.py
+++ articles/views.py
@@ -1,43 +1,41 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework import status
 from .models import Article
+from .serializer import ArticleSerializer
 
 @api_view(['GET'])
 def get_feed(request):
-    articles = Article.objects.all().values('id', 'title', 'description', 'rating')
-    return Response(list(articles))
+    """Возвращает список всех статей."""
+    articles = Article.objects.all()
+    serializer = ArticleSerializer(articles, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def get_article(request, article_id):
+    """Возвращает одну статью по её ID."""
     try:
         article = Article.objects.get(id=article_id)
     except Article.DoesNotExist:
-        return Response(
-            {"error": "Not found"},
-            status=status.HTTP_404_NOT_FOUND
-        )
+        return Response({"error": "Not found"}, status=status.HTTP_404_NOT_FOUND)
 
     return Response(article.to_json(), status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def create_artice(request):
-    title = request.data.get('title')
-    description = request.data.get('description')
-    rating = request.data.get('rating')
+    """
+    Создает новую статью.
+    Ожидает поля: title, description, rating.
+    """
+    serializer = ArticleSerializer(data=request.data)
+    if serializer.is_valid():
+        article = serializer.save()
+        return Response(ArticleSerializer(article).data, status=status.HTTP_201_CREATED)
 
-    if not title or not description or not rating:
-        return Response(
-            {"error": "Fields title, description, rating required"},
-            status = status.HTTP_400_BAD_REQUEST
-        )
-
-    article = Article.objects.create(title=title, description=description, rating=rating)
-
-    return Response(article.to_json(), status = status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['DELETE'])
 def delete_article(request, article_id):
+    """Удаляет статью по её ID."""
     try:
         article = Article.objects.get(id=article_id)
     except Article.DoesNotExist:
