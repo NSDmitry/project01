7e0a4cafe03ca08d63802f043fab4c7ba97b6eab
Dmitrij Serov <daserov1@mts.ru>
2025-01-28 23:34:10 +0300

    rename feed to articles

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import get_feed, create_artice, delete_article, get_article
+from .views import get_articles, create_artice, delete_article, get_article
 
 urlpatterns = [
-    path('api/feed', get_feed, name='get_feed'),
-    path('api/article', create_artice, name='create_new_article'),
+    path('api/articles', get_articles, name='get articles'),
+    path('api/article', create_artice, name='create article'),
     path('api/article/<int:article_id>', get_article, name="get article by id"),
     path('api/article/<int:article_id>', delete_article, name='delete article by id'),
 ]
