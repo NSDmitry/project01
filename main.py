3c8008acb9860f92a653659a3b2a885e2453bbb3
Dmitrij Serov <daserov1@mts.ru>
2025-02-02 12:28:41 +0300

    add user db

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from database import engine, Base
-from routers import articles
+from routers import SSO
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
-app.include_router(articles.router)
+app.include_router(SSO.router)
