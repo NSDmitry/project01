06b1193f356a5c6bd3246e791c0d9c77b66eba40
Dmitrij Serov <daserov1@mts.ru>
2025-02-22 13:15:03 +0300

    fixed user not found

--- services/User/UserService.py
+++ services/User/UserService.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from DBmodels import DBUser
@@ -15,5 +16,8 @@ class UserSerivce:
     @classmethod
     def get_user_by_id(cls, user_id: int, db: Session) -> PublicUserResponseModel:
         db_user: DBUser = db.query(DBUser).filter(DBUser.id == user_id).first()
+
+        if db_user is None:
+            raise HTTPException(status_code=404, detail="Пользователь с таким id не найден")
 
         return PublicUserResponseModel.from_db_model(db_model=db_user)
