81e1d04299f87342e6963cf8a0c84a6d4fe56be9
Dmitrij Serov <daserov1@mts.ru>
2025-02-23 14:29:03 +0300

    add relationship from user to bookclub

--- services/BookClub/Models.py
+++ services/BookClub/Models.py
@@ -1,8 +1,10 @@
-from pydantic import BaseModel
+from __future__ import annotations
 
+from typing import List
+
+from pydantic import BaseModel
 from DBmodels import DBBookClub
 from services.User.Models import PublicUserResponseModel
-from typing import List
 
 
 class CreateBookClubRequestModel(BaseModel):
@@ -12,8 +14,12 @@ class CreateBookClubRequestModel(BaseModel):
 class BookClubResponseModel(BaseModel):
     name: str
     description: str
-    owner_id: int
+    owner: PublicUserResponseModel
+    members: List[PublicUserResponseModel]
 
     @classmethod
-    def from_db_model(cls, db_model: DBBookClub) -> PublicUserResponseModel:
-        return BookClubResponseModel(name=db_model.name, description=db_model.description, owner_id=db_model.owner_id)
+    def from_db_model(cls, db_model: DBBookClub) -> BookClubResponseModel:
+        owner = PublicUserResponseModel.from_db_model(db_model=db_model.owner)
+        members = [PublicUserResponseModel.from_db_model(db_model=user) for user in db_model.members]
+
+        return BookClubResponseModel(name=db_model.name, description=db_model.description, owner=owner, members=members)
