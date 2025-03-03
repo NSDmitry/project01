fd7eda57d62b8a012ca7ede45a8a97ee0c28566b
Dmitrij Serov <daserov1@mts.ru>
2025-03-03 11:48:48 +0300

    fixed relationship in responses for user and bookclub

--- services/User/Models.py
+++ services/User/Models.py
@@ -9,13 +9,17 @@ class PublicUserResponseModel(BaseModel):
     id: int
     name: str
     phone_number: int
+    owned_book_clubs_ids: list[int]
+    joined_book_clubs_ids: list[int]
 
     @classmethod
     def from_db_model(cls, db_model: DBUser) -> PublicUserResponseModel:
         return PublicUserResponseModel(
             id=db_model.id,
             name=db_model.name,
-            phone_number=db_model.phone_number
+            phone_number=db_model.phone_number,
+            owned_book_clubs_ids= [club.id for club in db_model.owned_clubs],
+            joined_book_clubs_ids = [club.id for club in db_model.book_clubs]
         )
 
 class UpdateUserRequestModel(BaseModel):
