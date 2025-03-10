584420ac629c5a56a2fdec02b01810f62c44fecc
Dmitrij Serov <daserov1@mts.ru>
2025-03-10 16:13:43 +0300

    added member ids

--- DBmodels/DBBookClub.py
+++ DBmodels/DBBookClub.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
 from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.dialects.postgresql import ARRAY
+
 from database import Base
 
 class DBBookClub(Base):
@@ -11,3 +13,4 @@ class DBBookClub(Base):
     description = Column(String, nullable=False)
     creation_date = Column(DateTime, default=datetime.utcnow, nullable=False)
     owner_id = Column(Integer, nullable=False)
+    members_ids = Column(ARRAY(Integer), nullable=False, default=[])
