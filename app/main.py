d716ca08fce9d021bbaa69e8661347a7562cff64
Dmitrij <IDmitrySerov@gmail.com>
2025-05-21 08:48:07 +0300

    Move origin urls to env config
    * move origin in env

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from app.core.errors.APIExeption import APIException
 from app.db.database import engine, Base
 from app.api.routers import sso, users, book_club, discussions
 from fastapi.middleware.cors import CORSMiddleware
+from app.settings import settings
 
 app = FastAPI()
 
@@ -14,16 +15,9 @@ app.include_router(users.router)
 app.include_router(book_club.router)
 app.include_router(discussions.router)
 
-origins = [
-    "https://nsdmitrij.ru",
-    "http://localhost:5173",
-    "http://127.0.0.1:5173"
-]
-
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=settings.origin_urls,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
