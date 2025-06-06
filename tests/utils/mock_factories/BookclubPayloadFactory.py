d01f1aebc6d152438c5e3ffcc49948869dae9e7b
NSDmitry <idmitryserov@gmail.com>
2025-06-06 15:28:07 +0300

    fixed bookclub description in tests

--- tests/utils/mock_factories/BookclubPayloadFactory.py
+++ tests/utils/mock_factories/BookclubPayloadFactory.py
@@ -5,7 +5,7 @@ faker = Faker()
 
 class BookclubPayloadFactory:
     @staticmethod
-    def create_bookclub_payload(name: str = faker.pystr(min_chars=4, max_chars=99), description: str = faker.pystr(min_chars=4, max_chars=999)) -> Dict:
+    def create_bookclub_payload(name: str = faker.pystr(min_chars=4, max_chars=99), description: str = faker.pystr(min_chars=4, max_chars=499)) -> Dict:
         return {
             "name": name,
             "description": description
