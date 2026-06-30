713c8b413d34cbbbb37ab004b4a539d7ea2dafd3
Dmitrij <IDmitrySerov@gmail.com>
2026-06-30 15:47:41 +0300

    Feat/async (#35)

--- app/db/repositories/discussion_repository.py
+++ app/db/repositories/discussion_repository.py
@@ -1,6 +1,7 @@
 from typing import List
 
-from sqlalchemy.orm import Session
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.errors.errors import NotFound
 from app.db.models.db_discussion import DBDiscussion
@@ -8,25 +9,26 @@ from app.schemas.discussions_schema import DiscussionCreateRequestModel
 
 
 class DiscussionRepository:
-    db: Session
+    db: AsyncSession
 
-    def __init__(self, db: Session) -> None:
+    def __init__(self, db: AsyncSession) -> None:
         self.db = db
 
-    def get_discussions(self, book_club_id: int) -> List[DBDiscussion]:
-        discussions = self.db.query(DBDiscussion).filter(DBDiscussion.club_id == book_club_id).all()
+    async def get_discussions(self, book_club_id: int) -> List[DBDiscussion]:
+        result = await self.db.execute(select(DBDiscussion).where(DBDiscussion.club_id == book_club_id))
 
-        return discussions
+        return result.scalars().all()
 
-    def get_discussion(self, discussion_id: int) -> DBDiscussion:
-        discussion = self.db.query(DBDiscussion).filter(DBDiscussion.id == discussion_id).first()
+    async def get_discussion(self, discussion_id: int) -> DBDiscussion:
+        result = await self.db.execute(select(DBDiscussion).where(DBDiscussion.id == discussion_id))
+        discussion = result.scalar_one_or_none()
 
         if not discussion:
             raise NotFound(errors=["Обсуждение с таким id не найдено"])
 
         return discussion
 
-    def create_discussion(self, author_id: int, model: DiscussionCreateRequestModel) -> DBDiscussion:
+    async def create_discussion(self, author_id: int, model: DiscussionCreateRequestModel) -> DBDiscussion:
         new_discussion = DBDiscussion()
         new_discussion.club_id = model.club_id
         new_discussion.author_id = author_id
@@ -34,24 +36,25 @@ class DiscussionRepository:
         new_discussion.content = model.content
 
         self.db.add(new_discussion)
-        self.db.commit()
-        self.db.refresh(new_discussion)
+        await self.db.commit()
+        await self.db.refresh(new_discussion)
 
         return new_discussion
 
-    def delete_discussion(self, discussion_id: int) -> DBDiscussion:
-        discussion = self.db.query(DBDiscussion).filter(DBDiscussion.id == discussion_id).first()
+    async def delete_discussion(self, discussion_id: int) -> DBDiscussion:
+        result = await self.db.execute(select(DBDiscussion).where(DBDiscussion.id == discussion_id))
+        discussion = result.scalar_one_or_none()
         if discussion:
-            self.db.delete(discussion)
-            self.db.commit()
+            await self.db.delete(discussion)
+            await self.db.commit()
 
         return discussion
 
-    def update_discussion(self, discussion: DBDiscussion, model: DiscussionCreateRequestModel) -> DBDiscussion:
+    async def update_discussion(self, discussion: DBDiscussion, model: DiscussionCreateRequestModel) -> DBDiscussion:
         discussion.title = model.title
         discussion.content = model.content
 
-        self.db.commit()
-        self.db.refresh(discussion)
+        await self.db.commit()
+        await self.db.refresh(discussion)
 
         return discussion
